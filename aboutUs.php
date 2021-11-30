c9ba101631e4413915968ee004fd15fcb423f8f6
unknown <AnuSajeevan>
2021-11-30 13:33:51 +0530

    Aboutus in New Test branch

---
+++ aboutUs.php
@@ -0,0 +1,42 @@
+<html lang="en">
+	<head>
+		<!--using external files-->
+		<?php require('import.html') ?>
+		
+		<title>U Chen  Daily | About Us</title>
+	</head>
+	<body>
+		<!--using external files-->
+		<?php require('header.html'); ?>
+		<div class="container" style="margin: 20px;">
+			<div class="row">
+				<div class="col-sm"><h3 style="color: #1f52a3;">About Us</h3></div>
+			</div>
+		</div>
+		<div class="container-fluid bg-light" style="padding: 30px 10px;">
+			<div class="row bg-light" style="margin: 0 35px;">
+				<div class="col">
+					<h4>U Chen  Daily</h4>
+				</div>
+			</div>
+			<div class="row bg-light" style="margin: 10px 35px;">
+				<div class="col-6">
+					<p>Since its launch on June 23, 1995, as Malaysia's first news website, U Chen  Daily has always striven to provide readers with up to date breaking news, compelling content and insightful opinions.</p>
+					<p>Evolving with the times, our content includes current news, business, sports, community, tech, lifestyle and world news, as well as expert analysis and dynamic videos.</p>
+					<p>The website was recognised in 2014 as one of the best in Asia by the World Association of Newspapers and News Publishers (WAN-IFRA).</p>
+					<p>It serves as the foundation of an online community that has a million strong following on both Twitter and Facebook.</p>
+					<p>As the digital companion to U Chen  Daily newspaper, U Chen  Daily is part of a portfolio that offers content through print, U Chen  Daily ePaper and a mobile app.</p>
+					<p>Readers on-the-go who want breaking news and business updates delivered to them can opt for our SMS services available via Maxis and DiGi.</p>
+				</div>
+				<div class="col-6">
+					<p>U Chen  Daily (daily) and Sunday U Chen  are published in five editions. Two editions cover the northern peninsular states of Penang, Kedah, Perlis, Kelantan and northern Perak, while another two editions cover the rest of the country. As of March 2010, the newspaper has a separate Sarawak edition priced at RM1.20.</p>
+					<p>There are two main printing plants that publish four editions of U Chen  Daily on a daily basis. The northern editions are printed at U Chen  Daily Northern Hub in Bayan Lepas, Penang, while the other two editions are printed at U Chen  Daily Media Hub in Bukit Jelutong, Shah Alam, Selangor.</p>
+					<p>U Chen  Daily weekday paper is packaged as a 4-in-1 paper, comprising the Main Paper, U Chen Biz, U Chen 2 and U Chen  Metro. Naturally, this newspaper also contains classifieds.</p>
+					<p>The Main Paper covers the latest in both local and international news while U Chen Biz offers a comprehensive coverage of business developments, market trends, financial reports and updates in the stock market. U Chen 2 features articles on lifestyle, entertainment, health, parenting, social etiquette, science, environment, fashion, food, comics and many more. The contents published on U Chen  Daily Metro varies by edition, covering news and events in a particular region covered by each edition.</p>
+				</div>
+			</div>
+		</div>
+		<!--using external files-->
+		<?php require('footer.html'); ?>
+	</body>
+</html>
