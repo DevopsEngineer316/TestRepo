4803e7e8957e678c479788da639dc406aed001fa
DevopsEngineer316 <anusajeevan100@gmail.com>
2022-01-28 05:37:54 +0000

    autoupdated 2022-01-28-05:37:54

--- autopush.sh
+++ autopush.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-#file= git diff --name-only
-
-#echo "$file autoupdated `date +%F-%T`"
 file= git diff --name-only
-echo $file
+
+echo $file  
+
+
 DBUSER=gituser
 
 DBPASS=Gitpassword1!
 
 DBNAME=Testgitdatabase
 
+content= git diff
 
-
-#time= $(date +"%H:%M:%S")
-#conflicts_content = git diff
+echo $content
 
 mysql --user=$DBUSER --password=$DBPASS $DBNAME <<EOF
 use $DBNAME
-INSERT INTO conflicts(id, filename, content, time) VALUES (NULL, "'$file'" , "test", now());
+INSERT INTO conflicts(id, filename, content, time) VALUES (NULL, '$file', '$content', now());
 EOF
 
-git add .
+git add 
 git commit -a -m "$file autoupdated `date +%F-%T`"
 git push origin main
 
