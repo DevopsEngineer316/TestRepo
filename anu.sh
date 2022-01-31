a848362b4af0aa9e0338f309c6df14f3b1958d47
AnuSajeevan <anusajeevan100@gmail.com>
2022-01-31 05:50:49 +0000

    autoupdated 2022-01-31-05:50:49

--- anu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-file= git diff --name-only
-
-echo "$file autoupdated `date +%F-%T`"
--- jani.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-file= git diff --name-only
-echo $file
-
-git commit -a -m "autoupdate $file `date +%F-%T`"
