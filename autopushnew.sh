9fbce09436df3a2f3b5a96d09fb56d9230f4b24b
AnuSajeevan <annamalai.sivarasu@gmail.com>
2022-02-04 10:19:04 +0000

    autoupdated 2022-02-04-10:19:04

--- autopushnew.sh
+++ autopushnew.sh
@@ -33,7 +33,8 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    echo "There is a merge conflict. Aborting"
    merge_conflict="There is a merge conflict. Aborting"
    git merge --abort
-   exit 1
+
+   f
    else
      echo "no prob"
      varstatus_pull=$(git pull origin main 2>&1)
