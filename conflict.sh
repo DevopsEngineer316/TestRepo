6baedd0fe3d083b2e8c8c98ff92d1f40c6a7b213
AnuSajeevan <annamalai.sivarasu@gmail.com>
2022-02-07 14:08:02 +0000

    autoupdated 2022-02-07-14:08:02

--- conflict.sh
+++ conflict.sh
@@ -43,8 +43,8 @@ git add .
 echo $untrackedfile
 git commit -a -m " file autoupdated `date +%F-%T`" 
 if [ -z "$untrackedfile" ]; then
-#echo "New files are uploaded in gitHub"
-git push origin main
+echo "New files are uploaded in gitHub"
+#git push origin main
 #echo "push completed"
 else
 echo "Push Aborted"
