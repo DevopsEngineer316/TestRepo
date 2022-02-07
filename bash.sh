3b792b62c501c087d0dc6b22d6a76757ac3e967f
AnuSajeevan <annamalai.sivarasu@gmail.com>
2022-02-07 14:48:30 +0000

    file autoupdated 2022-02-07-14:48:30

--- bash.sh
+++ bash.sh
@@ -7,8 +7,8 @@ echo $untrackedfile
 git commit -a -m " file autoupdated `date +%F-%T`"
 if [ -z "$untrackedfile" ]; then
 echo "New files are uploaded in gitHub"
-#git push origin main
-#echo "push completed"
+git push origin main
+echo "push completed"
 else
 echo "Push Aborted"
 fi
