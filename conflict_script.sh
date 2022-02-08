bb4ab7655f004936b0a33b040e5dea8d3e0905da
AnuSajeevan <annamalai.sivarasu@gmail.com>
2022-02-08 10:52:47 +0000

    autoupdated 2022-02-08-10:52:47

--- conflict_script.sh
+++ conflict_script.sh
@@ -23,11 +23,10 @@ varstatus_pull=$(git pull origin main 2>&1)
 varstatus_cmt=$(git commit -a -m "$file autoupdated `date +%F-%T`" 2>&1)
 
 
-if [ -z "$file" ]; then
+if [ -n "$file" ]
+then
+
 mysqldump --no-tablespaces -u $DBUSER -p$DBPASS $DBNAME > $DBPATH/$DBNAME-$(date +%F-%T).sql
-
-
-
 mysql --user=$DBUSER --password=$DBPASS $DBNAME <<EOF
 use $DBNAME
 INSERT INTO conflicts(id, filename, content, time) VALUES (NULL, "$varstatus_pull $varstatus_cmt ", "content", now());
