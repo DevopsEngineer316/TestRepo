2fa564f613708b6dff178385664e3cd9c8c4a1fb
AnuSajeevan <annamalai.sivarasu@gmail.com>
2022-02-08 13:09:40 +0000

    autoupdated 2022-02-08-13:09:40

--- conflict.sh
+++ conflict.sh
@@ -26,6 +26,14 @@ varstatus_cmt=$(git commit -a -m "$file autoupdated `date +%F-%T`" 2>&1)
 if [ -n "$file" ]
 then
 
+
+	echo "New files are uploaded in gitHub"
+	git push origin main --force
+	echo "push completed"
+
+
+else
+
 mysqldump --no-tablespaces -u $DBUSER -p$DBPASS $DBNAME > $DBPATH/$DBNAME-$(date +%F-%T).sql
 mysql --user=$DBUSER --password=$DBPASS $DBNAME <<EOF
 use $DBNAME
@@ -38,7 +46,7 @@ git checkout main
 git merge Newbranch
 echo "Conflict occurs"
 
-else
+#else
 
 #checks for any new files created
 #untrackedfile= git ls-files --others --exclude-standard
@@ -50,9 +58,9 @@ else
 #git commit -a -m " file autoupdated `date +%F-%T`" 
 #if [ -z "$untrackedfile" ]
 #then
-echo "New files are uploaded in gitHub"
-git push origin main --force
-echo "push completed"
+#echo "New files are uploaded in gitHub"
+#git push origin main --force
+#echo "push completed"
 #fi
 
 fi
