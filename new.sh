096c4f0862a8d153af88079686185ee3119f52c3
AnuSajeevan <annamalai.sivarasu@gmail.com>
2022-02-08 07:06:23 +0000

    file autoupdated 2022-02-08-07:06:23

--- new.sh
+++ new.sh
@@ -4,11 +4,11 @@ git checkout Newbranch
 git add .
 echo $untrackedfile
 git commit -a -m " file autoupdated `date +%F-%T`"
-if [[ -z "$untrackedfile" ]]
-then
-  git push origin Newbranch 
-  echo "New files are uploaded in gitHub"
+#if [[ -z "$untrackedfile" ]]
+#then
+#  git push origin Newbranch 
+#  echo "New files are uploaded in gitHub"
 #else
 #  echo "Push aborted"
-fi
+#fi
 
