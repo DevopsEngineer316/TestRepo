de2617592d67f709cd2ec74277f669e6ca825faf
AnuSajeevan <annamalai.sivarasu@gmail.com>
2022-02-11 05:51:55 +0000

    file autoupdated 2022-02-11-05:51:55

--- hello.sh
+++ hello.sh
@@ -3,12 +3,14 @@ untrackedfile= git ls-files --others --exclude-standard
 git add .
 echo $untrackedfile
 git commit -a -m " file autoupdated `date +%F-%T`"
-if [ "$untrackedfile" != NULL ]
-then
-  git push origin main 
-  echo "New files are uploaded in gitHub"
-else
-  echo "No new files"
+#if [ "$untrackedfile" != NULL ]
+
+if [ -z "$untrackedfile" ] && echo "NULL" || echo "Not NULL"
+#then
+#  git push origin main 
+#  echo "New files are uploaded in gitHub"
+#else
+#  echo "No new files"
    
 fi
 
